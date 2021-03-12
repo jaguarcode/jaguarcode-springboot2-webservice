8c19080ec320863fe23f1e0d82995c7fde1202e1
IKHYEON KIM <behonz@gmail.com>
2021-03-12 20:52:58 +0900

    'Applying below' - JPA with H2 - application.properties

---
+++ src/test/java/com/jaguarcode/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.jaguarcode.book.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void load_post() {
+        //given
+        String title = "Test Post";
+        String content = "Test Content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("behonz@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
